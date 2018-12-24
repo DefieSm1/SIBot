6eb23c528857d3a5032af4f636673f5af9694f7b
Adam Grabski <adamg99@o2.pl>
2018-12-24 21:54:39 +0100

    Stopped unnecessarily importing the whole asyncio module

--- src/SIbot.py
+++ src/SIbot.py
@@ -1,8 +1,8 @@
 
 import discord  # needed for this whole thing to work in the first place
 from discord.ext import commands  # like above
-import os  # remove this line, it's only so my bot token stays private or keep it if you want to do the same
-import asyncio  # needed only for sleep function
+import os  # this if only needed for environ.get so the bot token stays private as an environmental variable
+from asyncio import sleep  # self explanatory
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('///'))  # adds the prefix needed to recognize commands
 
@@ -40,7 +40,7 @@ async def scare(ctx, channel: discord.VoiceChannel):
     vc = await channel.connect()  # connects to the chosen channel
     vc.play(discord.FFmpegPCMAudio('scare.mp3'), after=lambda e: print('done', e))  # plays scare.mp3
 
-    await asyncio.sleep(6)  # had to replace a loop with this because of a bug that would make to bot go half-offline
+    await sleep(6)  # if you're using a custom MP3, change the number to the duration of it (in seconds)
 
     await vc.disconnect()  # disconnects from the voice channel
 
